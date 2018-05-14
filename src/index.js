da65b702972226ab35165adc5355c0d77072837e
Giulio Maria Facin <giulio.maria@gmail.com>
2018-05-14 13:19:31 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import 'semantic-ui-css/semantic.css';
 import RockPaperScissors from './rock-paper-scissors';
 
+import 'semantic-ui-css/semantic';
+
 const rps = new RockPaperScissors(document.querySelector(".my-component"));
